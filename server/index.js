bac79ea003b5a8733b9161f676c00383391e3740
thanhantran1 <thanhantran21@gmail.com>
2022-05-14 17:37:02 +0700

    Add mongoDB on server side

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connect to mongoDB.");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -8,5 +25,6 @@ app.get('/', (req, res) => {
 
 const PORT = 5000
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`)
+    connect();
+    console.log(`Server running at http://localhost:${PORT}`)
 })
