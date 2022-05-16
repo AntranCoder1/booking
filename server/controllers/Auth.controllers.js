04d5ed07e9029f676515b22aa6dbc4698af482e8
thanhantran1 <thanhantran21@gmail.com>
2022-05-16 18:11:32 +0700

    Add function login and JWT with Cookie in Node.js

--- server/controllers/Auth.controllers.js
+++ server/controllers/Auth.controllers.js
@@ -1,6 +1,7 @@
 const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { createError } = require("../utils/error");
 
 module.exports.register = async (req, res, next) => {
 
@@ -22,5 +23,28 @@ module.exports.register = async (req, res, next) => {
 };
 
 module.exports.login = async (req, res, next) => {
-    
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) return next(createError(404, "User not found!"));
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!isPasswordCorrect)
+            return next(createError(404, "Wrong password or username!"));
+
+        const token = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.JWT
+        );
+
+        const { password, isAdmin, ...otherDetails } = user._doc;
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        }).status(200).json({ details: { ...otherDetails }, isAdmin });
+    } catch (error) {
+        next(error);
+    }
 };
