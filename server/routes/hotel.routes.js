48b0351a5b8105aa124e1213ae3729e3eacb3d2a
thanhantran1 <thanhantran21@gmail.com>
2022-05-16 16:13:23 +0700

    Add new function find hotel on server side

--- server/routes/hotel.routes.js
+++ server/routes/hotel.routes.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const hetelControllers = require("../controllers/Hotel.controllers");
+const hotelControllers = require("../controllers/Hotel.controllers");
 
 // @routes api/hotels/
 // @desc POST create new hotel
 // @access private
-router.post("/", hetelControllers.createHotel);
+router.post("/", hotelControllers.createHotel);
 
 // @routes api/hotels/:id
 // @desc PUT update hotel
 // @access private
-router.put("/:id", hetelControllers.updateHotel);
+router.put("/:id", hotelControllers.updateHotel);
 
 // @routes api/hotels/:id
 // @desc DELETE delete hotel
 // @access private
-router.delete("/:id", hetelControllers.deleteHotel);
+router.delete("/:id", hotelControllers.deleteHotel);
+
+// @routes api/hotels/:id
+// @desc GET get a hotel
+// @access private
+router.get("/:id", hotelControllers.getHotel);
 
 module.exports = router;
