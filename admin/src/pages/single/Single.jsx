835968651056be29d66a2f25a29370b93efb5255
thanhantran1 <thanhantran21@gmail.com>
2022-05-20 03:28:41 +0700

    Get user id in admin side

--- admin/src/pages/single/Single.jsx
+++ admin/src/pages/single/Single.jsx
@@ -3,8 +3,20 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const Single = () => {
+
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const userId = location.pathname.split('/')[2];
+
+  const user = useSelector(state => 
+    state.users.users.find((item) => item._id === userId)  
+  );
+
   return (
     <div className="single">
       <Sidebar />
@@ -16,29 +28,29 @@ const Single = () => {
             <h1 className="title">Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                src={user.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"}
                 alt=""
                 className="itemImg"
               />
               <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
+                <h1 className="itemTitle">{user.username}</h1>
                 <div className="detailItem">
                   <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemValue">{user.email}</span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Phone:</span>
-                  <span className="itemValue">+1 2345 67 89</span>
+                  <span className="itemValue">+{user.phone}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Address:</span>
+                  <span className="itemKey">City:</span>
                   <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
+                    {user.city}
                   </span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Country:</span>
-                  <span className="itemValue">USA</span>
+                  <span className="itemValue">{user.country}</span>
                 </div>
               </div>
             </div>
